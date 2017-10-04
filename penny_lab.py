75b2bc8b651d9ee309e611cce835e785165095b0
Brian Margolis <brian.m.margolis@gmail.com>
2017-10-04 18:00:49 -0500

    Improve CLI interactions

--- penny_lab.py
+++ penny_lab.py
@@ -1,52 +1,71 @@
 import csv
 import pickle
 import sys
+from getopt import getopt, GetoptError
 
 import cv2
 import numpy as np
 
-# penny_lab.py [image_path] [MIN_DIST_PENNIES] [MIN_RADIUS_PENNIES] [show_result]
-PIXEL_CACHE_LOCATION = 'pixel_cache.pickle'
-
-
-def bgr_to_rgb(color):
-    new_color = color.copy()
-    new_color[2] = color[0]
-    new_color[0] = color[2]
-    return new_color
+PIXEL_CACHE_FILE = 'pixel_cache.pickle'
+EXPORT_FILE = 'colors.csv'
 
 
 def main():
     # read in CLI args
+    verbose = False
+    scaling_factor = .5
+    try:
+        opts, args = getopt(sys.argv[1:], "hvs:")
+    except GetoptError:
+        display_help()
+        raise ValueError("Bad option flags.")
+    for opt, arg in opts:
+        if opt == '-h':
+            display_help()
+            sys.exit(0)
+        elif opt == '-v':  # verbose
+            verbose = True
+        elif opt == '-s':
+            scaling_factor = float(arg)
+
+    if len(sys.argv) < 4:
+        display_help()
+        raise ValueError("Not enough arguments.")
+
     image_path = sys.argv[1]
     image = cv2.imread(image_path)
-
-    min_dist = sys.argv[2]
-    min_radius = sys.argv[3]
-
-    show_result = sys.argv[4] == '1'
     if image is None:
         raise FileNotFoundError("No image found at: {0}. Exiting.".format(image_path))
 
-    print("Image is {0} by {1}\n\n.".format(image.shape[1], image.shape[0]))
+    if verbose:
+        print("Image found at {0} is {1} by {2}\n\n.".format(image_path, image.shape[1], image.shape[0]))
+
+    min_dist = sys.argv[2]
+    min_radius = sys.argv[3]
+    should_show_circles = sys.argv[4] == '1'
+
+    # Perform edge detection
     print("Detecting pennies...")
     circles = find_circles(image, min_dist, min_radius)
-    if circles is None:
+    circles = np.round(circles[0, :]).astype("int")
+    if circles is None or len(circles) == 0:
         print("No pennies found. Exiting.\n\n")
         exit(1)
 
-    circles = np.round(circles[0, :]).astype("int")  # bad code
     print("Found {0} pennies.\n\n".format(len(circles)))
-    for i, (x, y, r) in enumerate(circles):
-        print("Penny #{0}: (x, y) = ({1}, {2}), r = {3}.".format(i, x, y, r))
-    print("\n\n")
+    if verbose:
+        circles = np.round(circles[0, :]).astype("int")
+        for i, (x, y, r) in enumerate(circles):
+            print("Penny #{0}: (x, y) = ({1}, {2}), r = {3}.".format(i, x, y, r))
+        print("\n\n")
 
-    if show_result:
+    if should_show_circles:
         show_circles(circles, image, image_duration=5000)
 
-    circles, image = scale(circles, image, .5)
-    colors = analyze_color(circles, image)
-    with open('colors.csv', 'w+') as f:
+    circles, image = scale(circles, image, scaling_factor)
+    colors = analyze_color(circles, image, verbose)
+    print("Writing colors in RGB format to {0}.".format(EXPORT_FILE))
+    with open('%s' % EXPORT_FILE, 'w+') as f:
         writer = csv.writer(f)
         writer.writerows(colors)
 
@@ -82,16 +101,20 @@ def show_circles(circles, image, image_duration=0):
     cv2.waitKey(image_duration)
 
 
-def analyze_color(circles, image):
-    # set up pixel cache
+def analyze_color(circles, image, verbose):
     pixel_cache = load_pixel_cache()
 
     image_height = image.shape[0]
     image_width = image.shape[1]
-    colors = np.zeros(shape=(len(circles), 3))
-    for i, (x_center, y_center, radius) in enumerate(circles):
-        print("Processing penny #{0} located at ({1}, {2}) with radius = {3}...".format(i, x_center, y_center, radius))
 
+    num_circles = len(circles)
+    colors = np.zeros(shape=(num_circles, 3))
+    for i, (x_center, y_center, radius) in enumerate(circles):
+        if verbose:
+            print("Processing penny #{0}/{1} located at ({2}, {3}) with radius = {4}..."
+                  .format(i + 1, num_circles, x_center, y_center, radius))
+        else:
+            print("Processing penny #{0}/{1}".format(i + 1, num_circles))
         pixels = get_pixels(x_center, y_center, radius, pixel_cache)
         save_pixel_cache(pixel_cache)
 
@@ -102,8 +125,8 @@ def analyze_color(circles, image):
                 color_sum = np.add(color, color_sum)
 
         colors[i] = bgr_to_rgb(np.divide(color_sum, len(pixels)))
-
-        print("Average color for penny #{0}: {1}\n".format(i, ', '.join(colors[i].astype(str))))
+        if verbose:
+            print("Average color for penny #{0}: {1}\n".format(i + 1, ', '.join(colors[i].astype(str))))
 
     return colors
 
@@ -138,15 +161,15 @@ def calculate_pixels(c_x, c_y, r):
 # Pixel cache
 def load_pixel_cache():
     try:
-        with open(PIXEL_CACHE_LOCATION, 'rb') as f:
+        with open(PIXEL_CACHE_FILE, 'rb') as f:
             return pickle.load(f)
     except FileNotFoundError:
-        open(PIXEL_CACHE_LOCATION, 'x')
+        open(PIXEL_CACHE_FILE, 'x')
         return {}
 
 
 def save_pixel_cache(pixel_cache):
-    with open(PIXEL_CACHE_LOCATION, 'wb') as f:
+    with open(PIXEL_CACHE_FILE, 'wb') as f:
         pickle.dump(pixel_cache, f)
 
 
@@ -154,7 +177,7 @@ def get_pixel_cache_key(c_x, c_y, r):
     return "x:{0}_y:{1}_r:{2}".format(c_x, c_y, r)
 
 
-# utility functions
+# Utility functions
 def scale(circles, image, scaling_factor):
     print("Scaling by {0}.".format(scaling_factor))
     circles = scale_circles(circles, scaling_factor)
@@ -168,6 +191,32 @@ def scale_image(image, scaling_factor):
 
 def scale_circles(circles, scaling_factor):
     return np.multiply(circles.copy(), scaling_factor)
+
+
+def bgr_to_rgb(color):
+    new_color = color.copy()
+    new_color[2] = color[0]
+    new_color[0] = color[2]
+    return new_color
+
+
+# UI
+
+
+def display_help():
+    print("Usage:\n")
+    print("penny_lab.py [image_path] [min_dist] [min_radius] [show_circles]\n\n")
+
+    print("Parameters:\n")
+    print("min_dist: minimum distance between two circles in shape detection, measured in pixels\n")
+    print("min_radius: minimum radius of a circle in shape detection, measured in pixels\n")
+    print("show_circles: whether an image of the circles drawn over the original image is shown\nn")
+
+    print("Optional flags:\n")
+    print("-s: manually specify the scaling factor that is applied between edge detection and color averaging. "
+          "Defaults to 0.5\n")
+    print("-v: verbose mode, print more info\n")
+    print("-h: show this help information\n")
 
 
 if __name__ == "__main__":
